e64850c46de82b0b11934a6b46526dbe0e3b7e77
Alec German <alec_german@yahoo.com>
2018-02-20 02:29:19 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Artist
   extend Memorable
-
+  
   attr_accessor :name
   attr_reader :songs
 
@@ -21,9 +21,9 @@ class Artist
     @@artists
   end
 
-  # def self.reset_all
-    # self.all.clear
-  # end
+  def self.reset_all
+    self.all.clear
+  end
 
   # def self.count
     # @@artists.count
